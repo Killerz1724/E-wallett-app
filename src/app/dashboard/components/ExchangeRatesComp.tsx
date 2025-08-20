6e2077a5275d51630e1aa62407a3143757943239
tejo <tejo.hananto@shopee.com>
2025-08-20 19:00:11 +0700

    feat: added active tab style for exchange rates section

--- src/app/dashboard/components/ExchangeRatesComp.tsx
+++ src/app/dashboard/components/ExchangeRatesComp.tsx
@@ -4,28 +4,33 @@ import React from "react";
 import ConverterContent from "./ConverterContent";
 
 export default function ExchangeRatesComp() {
+  const [tabActive, setTabActive] = React.useState(0);
   return (
     <div className="flex flex-col">
       <div className="flex border-gray-300 border-1 rounded-tl-2xl rounded-tr-2xl w-fit">
         <span
           className={clsxm(
             `py-3 px-5 border-r-1 font-semibold rounded-tl-2xl border-gray-300 cursor-pointer `,
-            `hover:bg-orange-400 hover:text-white transition-colors`
+            `hover:bg-orange-400 hover:text-white transition-colors`,
+            tabActive === 0 && "bg-orange-400 text-white"
           )}
+          onClick={() => setTabActive(0)}
         >
           Currency Converter
         </span>
         <span
           className={clsxm(
             `py-3 px-5 border-r-1 font-semibold rounded-tr-2xl border-gray-300 cursor-pointer `,
-            `hover:bg-orange-400 hover:text-white transition-colors`
+            `hover:bg-orange-400 hover:text-white transition-colors`,
+            tabActive === 1 && "bg-orange-400 text-white"
           )}
+          onClick={() => setTabActive(1)}
         >
           Exchange Rates
         </span>
       </div>
       <div className="w-full border-1  border-gray-300 rounded-tr-2xl rounded-b-2xl px-5 py-4">
-        <ConverterContent />
+        {tabActive === 0 && <ConverterContent />}
       </div>
     </div>
   );
