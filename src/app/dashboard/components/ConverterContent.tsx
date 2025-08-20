dfcd5246a6e2da519e9507c343b6a5e53ef22474
tejo <tejo.hananto@shopee.com>
2025-08-20 17:42:23 +0700

    feat: added notes on convert tab

--- src/app/dashboard/components/ConverterContent.tsx
+++ src/app/dashboard/components/ConverterContent.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FromCountry from "./FromCountry";
 import ToCountry from "./ToCountry";
+import { ArrowLeftRight } from "lucide-react";
 
 export default function ConverterContent() {
+  const result = 500000;
   return (
     <>
-      <div className="flex gap-4 items-end">
+      <div className="flex flex-col gap-8 ">
         <div className="space-y-4 w-full">
           <h3 className="font-semibold text-lg">Input Currency Amount</h3>
           <div className="flex gap-3 items-center">
@@ -14,9 +16,24 @@ export default function ConverterContent() {
               placeholder="0.00"
             />
             <FromCountry />
+            <ArrowLeftRight />
             <ToCountry />
           </div>
         </div>
+        <div className="space-y-3">
+          <span className="font-semibold text-sm text-gray-400">1 USD = </span>
+          <p className="text-4xl font-bold">
+            Rp. {result.toLocaleString("id-ID")}
+          </p>
+          <span className="font-semibold text-sm text-gray-400">
+            1 USD = 16.000 IDR{" "}
+          </span>
+        </div>
+        <p className="text-sm text-gray-400 max-w-lg text-justify">
+          We use the mid-market rate for our Converter. This is for
+          informational purposes only. You won’t receive this rate when sending
+          money.
+        </p>
       </div>
     </>
   );
