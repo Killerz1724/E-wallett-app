fd2279dae26e830e0ba00f997e20edfec83d9f7d
tejo <tejo.hananto@shopee.com>
2025-08-22 15:36:09 +0700

    refac: change transfer select to server select

--- src/app/transferModal/components/SelectMethod.tsx
+++ src/app/transferModal/components/SelectMethod.tsx
@@ -1,5 +1,6 @@
 "use client";
-import Select, { StylesConfig } from "react-select";
+
+import SelectServer from "components/SelectServer";
 
 const dummyOption = [
   { value: "a", label: "a" },
@@ -7,38 +8,9 @@ const dummyOption = [
   { value: "c", label: "c" },
 ];
 
-const customStyles: StylesConfig = {
-  option: (provided, state) => ({
-    ...provided,
-    backgroundColor: state.isSelected
-      ? "#f97316"
-      : state.isFocused
-      ? "orange"
-      : "white",
-    color: state.isSelected ? "white" : state.isFocused ? "white" : "black",
-    borderRadius: "0.5rem",
-    cursor: "pointer",
-  }),
-  control: (provided) => ({
-    ...provided,
-    borderRadius: "0.5rem",
-    borderColor: "#e5e7eb",
-    boxShadow: "none",
-    "&:hover": {
-      borderColor: "#f97316",
-    },
-  }),
-};
-
 export default function SelectRecipient() {
   function handleChange() {
     console.log("change");
   }
-  return (
-    <Select
-      options={dummyOption}
-      styles={customStyles}
-      onInputChange={handleChange}
-    />
-  );
+  return <SelectServer options={dummyOption} onInputChange={handleChange} />;
 }
