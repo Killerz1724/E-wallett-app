8af9a34d3d236dcf0196810ac5b30a9a87a5d5d7
tejo <tejo.hananto@shopee.com>
2025-08-22 14:51:07 +0700

    feat: added react select

--- src/app/transferModal/components/SelectMethod.tsx
+++ src/app/transferModal/components/SelectMethod.tsx
@@ -1,18 +1,44 @@
-import { Popover, PopoverContent, PopoverTrigger } from "components/ui/Popover";
-import React from "react";
+"use client";
+import Select, { StylesConfig } from "react-select";
+
+const dummyOption = [
+  { value: "a", label: "a" },
+  { value: "b", label: "b" },
+  { value: "c", label: "c" },
+];
+
+const customStyles: StylesConfig = {
+  option: (provided, state) => ({
+    ...provided,
+    backgroundColor: state.isSelected
+      ? "#f97316"
+      : state.isFocused
+      ? "orange"
+      : "white",
+    color: state.isSelected ? "white" : state.isFocused ? "white" : "black",
+    borderRadius: "0.5rem",
+    cursor: "pointer",
+  }),
+  control: (provided) => ({
+    ...provided,
+    borderRadius: "0.5rem",
+    borderColor: "#e5e7eb",
+    boxShadow: "none",
+    "&:hover": {
+      borderColor: "#f97316",
+    },
+  }),
+};
 
 export default function SelectRecipient() {
+  function handleChange() {
+    console.log("change");
+  }
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <input type="text" placeholder="select recepient" />
-      </PopoverTrigger>
-      <PopoverContent>
-        <ul>
-          <li>Loki</li>
-          <li>Thor</li>
-        </ul>
-      </PopoverContent>
-    </Popover>
+    <Select
+      options={dummyOption}
+      styles={customStyles}
+      onInputChange={handleChange}
+    />
   );
 }
